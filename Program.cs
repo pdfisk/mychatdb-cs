4a3a78e438a8cf35ecf180ac8f2bcf8f8e5ce870
Peter Fisk <peter.fisk@gmail.com>
2025-10-17 10:13:46 -0400

    transcript

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace MyChatDB
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Transcript());
         }
     }
 }
--- windows/Transcript.Designer.cs
+++ windows/Transcript.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MyChatDB
 {
-    partial class Form1
+    partial class Transcript
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Transcript
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(614, 450);
+            this.Name = "Transcript";
+            this.Text = "Transcript";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
