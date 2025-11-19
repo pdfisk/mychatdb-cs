dd14e16aae0b791cf7ee218d4fbd723ecf038c47
Peter Fisk <peter.fisk@gmail.com>
2025-11-19 12:33:22 -0500

    object inspector

--- src/windows/inspectors/ObjectInspector.cs
+++ src/windows/inspectors/ObjectInspector.cs
@@ -5,9 +5,25 @@ namespace MyChatDB.src.windows.inspectors
 {
     public partial class ObjectInspector : Form
     {
+        Dictionary<string, object> _dictionary;
+
         public ObjectInspector(Dictionary<string, object> dictionary)
         {
             InitializeComponent();
+            _dictionary = dictionary;
+            refreshList();
+        }
+
+        void refreshList()
+        {
+            listBox.Items.Clear();
+            List<string> keys = new List<string>(_dictionary.Keys);
+            keys.Sort();
+            foreach (string key in keys)
+            {
+                object value = _dictionary[key];
+                listBox.Items.Add(key);
+            }
         }
     }
 }
